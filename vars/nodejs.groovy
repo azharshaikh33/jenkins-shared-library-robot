02af0ef9f597873ccc9602c82ed4aebaa110d21d
Azhar <azharshaikh33@gmail.com>
2024-05-05 14:48:30 +0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -17,7 +17,7 @@ def call() {
             stage('Lint CHeck') {
                 steps {
                     script {
-                        lintchecks
+                        lintchecks ()
                     }
                 }
             }
