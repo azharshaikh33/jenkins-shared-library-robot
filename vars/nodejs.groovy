ede083270a6776694d2a79bdd87dd8a4e3ae3723
Azhar <azharshaikh33@gmail.com>
2024-05-05 14:46:33 +0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,12 +1,12 @@
 def lintchecks() {
     sh '''
-            sh "echo lint checks for ${COMPONENT}"
-            sh "echo Installing JSlist"
-            sh "npm i jslint"
-            sh "ls -ltr node_modules/jslint/bin/"
-            sh "node_modules/jslint/bin/jslint.js server.js"
-            sh "echo performing lint checks"
-            sh "echo performing lint checks completed"
+            sh echo lint checks for ${COMPONENT}
+            sh echo Installing JSlist
+            sh npm i jslint
+            sh ls -ltr node_modules/jslint/bin/
+            sh node_modules/jslint/bin/jslint.js server.js
+            sh echo performing lint checks
+            sh echo performing lint checks completed
     '''
 }
 
