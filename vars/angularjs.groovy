52e707dda5d3bce2559d2cc2bf524d91718996d1
Azhar <azharshaikh33@gmail.com>
2024-05-05 17:29:30 +0400

    Auto Push

---
+++ vars/angularjs.groovy
@@ -0,0 +1,26 @@
+def lintchecks() {
+    sh '''
+            echo lint checks for ${COMPONENT}
+            echo Installing JSlist
+            # sh npm i eslint
+            # sh ls -ltr node_modules/jslint/bin/
+            # sh node_modules/jslint/bin/jslint.js server.js
+            echo performing lint checks for ${COMPONENT}
+            echo performing lint checks completed for ${COMPONENT}
+    '''
+}
+
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Lint Check') {
+                steps {
+                    script {
+                        lintchecks ()
+                    }
+                }
+            }
+        }
+    }
+}
