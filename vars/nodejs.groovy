6679829aec1a8bd157b530996bd4d8f5909fc569
Azhar <azharshaikh33@gmail.com>
2024-05-07 09:45:26 +0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -32,6 +32,9 @@ def call() {
                 steps {
                     script {
                         sonarchecks ()
+                    }
+                }
+            }
         }
     }
 }
