76daf97654c1d6ce8e656483059d9950c503afe6
Azhar <azharshaikh33@gmail.com>
2024-05-09 16:10:23 +0400

    Auto Push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -20,12 +20,14 @@ def call() {
             }
 
             stage('Prepare the artifacts') {
+                when { expression { env.TAG_NAME != null } }
                 steps {
                     sh "echo prepare the artifacts"
                 }
             }
 
             stage('Publish the artifacts') {
+                when { expression { env.TAG_NAME != null } }
                 steps {
                     sh "echo publish the artifacts"
                 }
