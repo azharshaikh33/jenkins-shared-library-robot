5e10354420216fc5ca58366d1f40748723b60380
Azhar <azharshaikh33@gmail.com>
2024-05-14 01:54:03 +0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -72,7 +72,7 @@ def call() {
                 when { expression { env.TAG_NAME != null } }
                 steps {
                     sh "echo publish the artifacts"
-                    sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                    sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
                 }
             }
         }
