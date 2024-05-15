3e54f1d7b207a7ef49f040c2e406d4af5c29312a
Azhar <azharshaikh33@gmail.com>
2024-05-16 01:15:43 +0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,10 @@
 def sonarchecks() {
+    stage('Sonar Checks') {
     // sh "sonar-scanner -X -Dsonar.host.url=http://172.31.20.98:9000 ${ARGS} -Dsonar.projectkey=${COMPONENT} -Dsonar.login=$(SONAR_USR) -Dsonar.password=$(SONAR_PSW)"
     // curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
     // bash -x quality-gate.sh $(SONAR_PSW) $(SONAR_PSW) $(SONAR_URL) $(COMPONENT)
     sh "echo performing code quality check"
+    }
 }
 
 def testcases() {
